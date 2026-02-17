18758f57aef7a1175dbe650b2594a040471d3474
Tobias Sousa <100934496+tobiassousa@users.noreply.github.com>
2026-02-17 19:03:13 -0300

    Adição de dados e adição de alguns contadores

--- source/lista/views.py
+++ source/lista/views.py
@@ -6,19 +6,41 @@ from .models import Lista
 def criar_convidado(request):
     if request.method == 'GET':
         lista = Lista.objects.all()
-        return render(request, 'criar_convidado.html', {'lista':lista})
+        
+        total = Lista.objects.count()
+        total_sim = Lista.objects.filter(confirmado="Sim").count()
+        total_nao = Lista.objects.filter(confirmado="Não").count()
+        total_menor = Lista.objects.filter(idade_menor="Sim").count()
+        total_maior = Lista.objects.filter(idade_maio="Sim").count()
+
+        context = {
+            "lista": lista,
+            "total": total,
+            "total_sim": total_sim,
+            "total_nao": total_nao,
+            "total_menor":total_menor,
+            "total_maior":total_maior
+        }
+    
+        return render(request, 'criar_convidado.html', context)
     elif request.method == 'POST':
         nome = request.POST.get('nome')
         relacao = request.POST.get('relacao')
         confirmado = request.POST.get('confirmado')
+        idade_menor = request.POST.get('idade_menor')
+        idade_maio = request.POST.get('idade_maio')
         
         lista = Lista(
             nome = nome,
             relacao = relacao,
-            confirmado = confirmado
+            confirmado = confirmado,
+            idade_menor = idade_menor,
+            idade_maio = idade_maio
         )
         
         lista.save()
+        
+
         
         return redirect('criar_convidado')
     
@@ -35,10 +57,14 @@ def atualizar_convidado(request, id):
     nome = request.POST.get('nome')
     relacao = request.POST.get('relacao')
     confirmado = request.POST.get('confirmado')
+    idade_menor = request.POST.get('idade_menor')
+    idade_maio = request.POST.get('idade_maio')
     
     lista.nome = nome
     lista.relacao = relacao
     lista.confirmado = confirmado
+    lista.idade_menor = idade_menor
+    lista.idade_maio = idade_maio
     
     lista.save()
     
